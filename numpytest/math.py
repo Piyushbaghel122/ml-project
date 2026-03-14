3ea06560801cafd292c475d857dfcb5139cf1f95
piyush kumar <piyush kumar>
2026-03-14 23:47:57 +0530

    reversive

--- numpytest/math.py
+++ numpytest/math.py
@@ -2,4 +2,4 @@ import numpy as np
 
 arr = np.array([10, 20 ,30, 40 , 50])
 print(arr[1: 4])
-
+print(arr[::-1])
--- numpytest/slicing.py
+++ numpytest/slicing.py
@@ -1,4 +1,5 @@
 import numpy as np 
- 
-arr = np.array([12,10 , 20 , 30 , 40])
-print(arr[1:4])
+
+arr = np.array([10, 20 , 30 , 40 ,50])
+
+print(arr[2:4])
